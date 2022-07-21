9dc5b7c99408d212969bd1745530435e55562fcd
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-21 14:21:15 +0530

    Add presence validations and presence validations test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,10 +3,25 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(firstname: "newuser", email: "user@example.com")
+    @user = User.new(firstname: "new", lastname:"user", email: "user@example.com")
   end
 
   test "should be valid" do
     assert @user.valid?
+  end
+
+  test "firstname should be present" do
+    @user.firstname = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "lastname should be present" do
+    @user.lastname = "     "
+    assert_not @user.valid?
   end
 end
