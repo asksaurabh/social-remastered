ec84ea441684aabb7be83034df45326271708690
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-23 10:59:53 +0530

    Add create action to handle post request of submitting signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
+
+  # Action to get the signup page
   def new
     @user = User.new
   end
 
+  # Action to get the profile page of a valid user in DB
   def show
     @user = User.find(params[:id])
+  end
+
+  # Action to create a new user from signup page
+  def create
+    @user = User.new(params[:user])
+    if(@user.save)
+      # handle successful save
+    else
+      # Render the signup page(new.html.erb) with error messages
+      render 'new', status: :unprocessable_entity
+    end
   end
 end
