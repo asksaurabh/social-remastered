54d8fdc33b3730625faf6e5b3959941bca7a152d
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-24 22:24:30 +0530

    Refactor the retrieval of current_user if it exists

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,10 +7,7 @@ module SessionsHelper
 
   # Returns the current logged-in user(if any)
   def current_user
-    if @current_user.nil?
-      # Save the current user and returns it to avoid finding current loggedin user again and again
-      @current_user = User.find_by(id: session[:user_id])
-    else
-      @current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
   end
 end
