d2ad9e7c82cfd02c2e4e734dedfb05398753e316
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-24 22:19:16 +0530

    Get current logged in user if any

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,13 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # Returns the current logged-in user(if any)
+  def current_user
+    if @current_user.nil?
+      # Save the current user and returns it to avoid finding current loggedin user again and again
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      @current_user
+  end
 end
