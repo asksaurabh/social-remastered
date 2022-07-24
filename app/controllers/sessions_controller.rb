7f8f6266b2eb8183dde419e19491d04cb30eaa9d
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-24 21:15:08 +0530

    Handle valid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,11 @@ class SessionsController < ApplicationController
     
     # If user exists and it's password matches
     # !! -> returns the value in boolean context
-    if !!(user && user.authenticate(params[:session][:email]))
+    if !!(user && user.authenticate(params[:session][:password]))
       # Redirect to profile page of the user
-      
+      reset_session
+      log_in user
+      redirect_to user_url(user)
     else
       # Create an error message (Not quite right as flash persists)
       flash.now[:danger] = "Invalid email/password combination"
