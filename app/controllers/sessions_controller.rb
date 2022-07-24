c6965a540f1dd75bf18ea50472a7bfab2f85452a
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-24 18:44:43 +0530

    Add basic requirements to create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    
+    # If user exists and it's password matches
+    # !! -> returns the value in boolean context
+    if !!(user && user.authenticate(params[:session][:email]))
+      # Redirect to profile page of the user
+    else
+      # Create an error message
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
