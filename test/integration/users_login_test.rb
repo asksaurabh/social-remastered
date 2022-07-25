6dc83c06890e596b1b02371b5ce2ce83935100d4
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-25 10:49:46 +0530

    Add a test to simulate valid user login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  # Pulls a test account from users.yml(Fixtures)
+  def setup
+    @user = users(:saurabh)
+  end
+
   # A test to handle invalid user login
   test "login with invalid information" do 
     get login_path
@@ -12,5 +17,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  # Test to simulate valid login
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password'} }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
