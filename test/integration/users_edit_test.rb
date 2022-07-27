a0c54a82c0362f3a10f62aa29b4efb6618a99f2f
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-27 21:21:40 +0530

    Test to check if forwading url becomes nil after log_in

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -25,7 +25,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
     log_in_as(@user)
+    assert_nil session[:forwarding_url]
     assert_redirected_to edit_user_url(@user)
     new_fname = "Foo"
     new_email = "admin@gmail.com"
