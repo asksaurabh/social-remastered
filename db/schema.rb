e893b5ae6f88f868892ad0bd0c23e54b28941691
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-28 13:10:08 +0530

    Add admin attribute to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_055959) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_073909) do
   create_table "users", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_055959) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
