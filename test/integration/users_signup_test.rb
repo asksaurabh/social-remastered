2be5e78f9d0fbf52fde8d2798b8acf778002c379
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-30 02:25:20 +0530

    Refactor user signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
-class UsersSignupTest < ActionDispatch::IntegrationTest
+class UsersSignup < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+end
   
+class UsersSignupTest < UsersSignup
+
   test "invalid signup information" do
-    get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { firstname: "", 
+      post users_path, params: { user: { firstname:  "",
+                                         lastname: "d",
                                          email: "user@invalid",
-                                         password: "foo",
+                                         password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_response :unprocessable_entity
@@ -16,19 +23,54 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
-    get signup_path
+  test "valid signup information with account activation" do
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { id: "1",
-                                        firstname: "saurabh",
-                                        lastname: "Kumar",
-                                        email: "user@invalid.com",
-                                        password: "foobar",
-                                        password_confirmation: "foobar" } }
+      post users_path, params: { user: { firstname:  "Example User",
+                                         lastname: "Kr",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+  end
+end
+
+class AccountActivationTest < UsersSignup
+
+  def setup
+    super
+    post users_path, params: { user: { firstname:  "Example",
+                                       lastname: "User",
+                                       email: "user@example.com",
+                                       password:              "password",
+                                       password_confirmation: "password" } }
+    @user = assigns(:user)
+  end
+
+  test "should not be activated" do
+    assert_not @user.activated?
+  end
+
+  test "should not be able to log in before account activation" do
+    log_in_as(@user)
+    assert_not is_logged_in?
+  end
+
+  test "should not be able to log in with invalid activation token" do
+    get edit_account_activation_path("invalid token", email: @user.email)
+    assert_not is_logged_in?
+  end
+
+  test "should not be able to log in with invalid email" do
+    get edit_account_activation_path(@user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+  end
+
+  test "should log in successfully with valid activation token and email" do
+    get edit_account_activation_path(@user.activation_token, email: @user.email)
+    assert @user.reload.activated?
     follow_redirect!
-    # assert_template 'users/show'
-    # assert is_logged_in?                  # To check if the user is logged in after immediate signup
-    # assert_not flash.empty?
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
