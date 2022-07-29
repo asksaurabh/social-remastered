c5c7b962ff26bad34f83c7368fc5341ed47ddff3
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-29 14:30:19 +0530

    Add activation token attributes and before callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = self.email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :firstname, presence: true, length: { maximum: 50 }
   validates :lastname, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -51,4 +52,16 @@ class User < ApplicationRecord
       BCrypt::Password.new(remember_digest).is_password?(browser_remember_token)
     end
   end
+
+  private
+    # Converts email to all lowercase.
+    def downcase_email
+      self.email = email.downcase
+    end
+    
+    def create_activation_digest
+      # Create the token and digest.
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
