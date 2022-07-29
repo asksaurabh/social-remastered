5592b28775244c46e3ed4ecbe99cee1f67770d57
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-30 01:59:48 +0530

    Handle the activation link

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,7 +1,19 @@
 class AccountActivationsController < ApplicationController
 
+  # Handles the activation link
   def edit
     user = User.find_by(email: params[:email])
-    if user && user.authenticated?(:activation, params[:id])
+    if user && user.activated? == false && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+
+      log_in user
+
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
   end
 end
