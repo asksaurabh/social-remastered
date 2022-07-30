500ee0363608d1189991e33ef401fc7e8428e32a
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-30 11:08:33 +0530

    Setup smtp, credentials for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,15 +41,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   config.action_mailer.delivery_method = :test 
 
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => "devhere101@gmail.com",
-    :password             => "ypomqknqgcdemgni",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :user_name            => "devhere101@gmail.com",
+  #   :password             => "ypomqknqgcdemgni",
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }
 
   config.action_mailer.perform_caching = false
 
