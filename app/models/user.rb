9478d0e1e41343d3f0f1cf1ab887f07eb612a669
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-31 01:51:47 +0530

    Add a update action to save the new password

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,6 +65,12 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    # Password reset sent earlier than two hours ago.
+    reset_sent_at < 2.hours.ago
+  end
+
   private
     # Converts email to all lowercase.
     def downcase_email
