1d36ce6d553565602e47b36801c8a0ba94accffc
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-31 02:56:26 +0530

    Password reset complete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,17 +40,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
-  config.action_mailer.delivery_method = :smtp
-
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => "devhere101@gmail.com",
-    :password             => "ypomqknqgcdemgni",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :test
 
   config.action_mailer.perform_caching = false
 
