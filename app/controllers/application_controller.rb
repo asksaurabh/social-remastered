3959f346557a4de600c6b3f0b6021f936dac980f
Saurabh Kumar <asksaurabh101@gmail.com>
2022-07-31 23:05:05 +0530

    Add access control to Micropost controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
 
   include SessionsHelper
+
+  # Before filters
+  # Confirms if the user is logged-in or not
+  def logged_in_user
+    if logged_in? == false
+      store_location   # Capture the current state.(SessionHelper)
+      flash[:danger] = "Please log in"
+      redirect_to login_url, status: :see_other
+    end
+  end
 end
